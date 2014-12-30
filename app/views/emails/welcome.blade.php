2ba54b5e8dd6b3731a21888e1e89e819545cbcab
Thomas Jensen <thomas@dragonlancers.com>
2014-12-30 21:04:54 +0400

    changed form mails

--- app/views/emails/welcome.blade.php
+++ app/views/emails/welcome.blade.php
@@ -2,13 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome to Dragon Lancers! (NO REPLY)</title>
+    <title>Welcome to Dragon Lancers!</title>
 </head>
 <body>
 <h3>Dear {{$name}} </h3>
-<p> Thank you for inquiring us for our services, <br>
+<p> Thank you for inquiring us about our services, <br>
     We will get in touch with you shortly.<br> <br>
     Dragon Lancers <br> <br> <br>
-    DO NOT REPLY to this email as it is only one way</p>
+    Unfortunately, this email is an automated notification,
+    which is unable to receive replies, we will get back to you as soon as possible.
+    We are happy to help you with any questions or concerns you may have.
+    Please contact us directly 24/7 via http://www.dragonlancers.com/#contact or hello@dragonlancers.com.</p>
 </body>
 </html>
