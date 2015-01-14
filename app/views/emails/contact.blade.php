345cf378ecbf5f0e4aed693ccfc487ea063ae9ba
Thomas Jensen <thomas@dragonlancers.com>
2015-01-14 11:25:46 +0100

    added contact & welcome mails fixed 8th time

--- app/views/emails/contact.blade.php
+++ app/views/emails/contact.blade.php
@@ -2,46 +2,63 @@
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+    <meta name="generator" content="HTML Tidy for Linux (vers 25 March 2009), see www.w3.org" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>New inquiry from the website!</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <style type="text/css">
+        /*<![CDATA[*/
+        body.c12 {margin: 0; padding: 0;}
+        td.c11 {padding: 20px 0 30px 0;}
+        table.c10 {border-collapse: collapse;}
+        td.c9 {background-color: #000000; padding-left: 10%; padding-right: 10%}
+        table.c8 {color: #ffffff}
+        td.c7 {font-size: 0; line-height: 0;}
+        td.c6 {font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 14px; line-height: 20px;}
+        td.c5 {background-color: #FCFCFC; padding-left: 10%; padding-right: 10%}
+        td.c4 {padding-top: 5%; padding-bottom: 10%; font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 16px; line-height: 20px;}
+        td.c3 {font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 16px; line-height: 20px;}
+        td.c2 {padding-top: 5%; padding-bottom: 10%; font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 24px; line-height: 20px;}
+        img.c1 {display: block;}
+        /*]]>*/
+    </style>
 </head>
-<body style="{margin: 0; padding: 0;}">
+<body class="c12">
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
     <tr>
-        <td style="{padding: 20px 0 30px 0;}">
-            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="{border-collapse: collapse;}">
+        <td class="c11">
+            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" class="c10">
                 <tr>
-                    <td align="center"><img src="{{ URL::asset('assets/images/build/mailheader.jpg') }}" alt="Dragon Lancers Creative Agency" width="100%" style="{display: block;}" /></td>
+                    <td align="center"><img src="{{ URL::asset('assets/images/build/mailheader.jpg') }}" alt="Dragon Lancers Creative Agency" width="100%" class="c1" /></td>
                 </tr>
                 <tr>
-                    <td style="{background-color: #FCFCFC; padding-left: 10%; padding-right: 10%}">
+                    <td class="c5">
                         <table border="0" cellpadding="0" cellspacing="0" width="100%">
                             <tr>
-                                <td style="{padding-top: 5%; padding-bottom: 10%; font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 24px; line-height: 20px;}">New inquiry from the website!</td>
+                                <td class="c2">New inquiry from the website!</td>
                             </tr>
                             <tr>
-                                <td style="{font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 16px; line-height: 20px;}">From {{$name='Tea'}},</td>
+                                <td class="c3">From {{$name='Tea'}},</td>
                             </tr>
                             <tr>
-                                <td style="{padding-top: 5%; padding-bottom: 10%; font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 16px; line-height: 20px;}">{{$messages='messages'}}</td>
+                                <td class="c4">{{$messages='Messages'}}</td>
                             </tr>
                         </table>
                     </td>
                 </tr>
                 <tr>
-                    <td height="50" style="{background-color: #000000; padding-left: 10%; padding-right: 10%}">
-                        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="{color: #ffffff}">
+                    <td height="50" class="c9">
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%" class="c8">
                             <tr>
-                                <td width="75%" style="{font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 14px; line-height: 20px;}">© Dragon Lancers {{ date('Y') }}</td>
+                                <td width="75%" class="c6">© Dragon Lancers {{ date('Y') }}</td>
                                 <td width="25%" align="right">
                                     <table border="0" cellpadding="0" cellspacing="0">
                                         <tr>
                                             <td><a href="https://www.facebook.com/DragonLancers"><img src="{{ URL::asset("assets/images/build/facebook.png") }}" alt="Facebook" width="50" height="50" class="c1" border="0" /></a></td>
-                                            <td style="{font-size: 0; line-height: 0;}" width="20"> </td>
-                                            <td><a href="https://twitter.com/dragonlancersco"><img src="{{ URL::asset('assets/images/build/twitter.png') }}" alt="Twitter" width="50" height="50" class="c1" border="0" /></a></td>
-                                            <td style="{font-size: 0; line-height: 0;}" width="20"> </td>
-                                            <td><a href="https://plus.google.com/111624196448495342344"><img src="{{ URL::asset('assets/images/build/googleplus.png') }}" alt="Google+" width="50" height="50" class="c1" border="0" /></a></td>
+                                            <td class="c7" width="20"> </td>
+                                            <td><a href="https://twitter.com/dragonlancersco"><img src="{{ URL::asset("assets/images/build/twitter.png") }}" alt="Twitter" width="50" height="50" class="c1" border="0" /></a></td>
+                                            <td class="c7" width="20"> </td>
+                                            <td><a href="https://plus.google.com/111624196448495342344"><img src="{{ URL::asset("assets/images/build/googleplus.png") }}" alt="Google+" width="50" height="50" class="c1" border="0" /></a></td>
                                         </tr>
                                     </table>
                                 </td>
