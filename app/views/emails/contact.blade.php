251eabb04660ca7455e5ddbebe1032f4f5820091
Thomas Jensen <thomas@dragonlancers.com>
2015-01-14 10:49:58 +0100

    added contact & welcome mails fixed 4th time

--- app/views/emails/contact.blade.php
+++ app/views/emails/contact.blade.php
@@ -1,79 +1,57 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>New inquiry from the website!</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
-
-<body style="margin: 0; padding: 0;">
+<body style="{margin: 0; padding: 0;}">
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
     <tr>
-        <td style="padding: 20px 0 30px 0;">
-            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;">
+        <td style="{padding: 20px 0 30px 0;}">
+            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="{border-collapse: collapse;}">
                 <tr>
-                    <td align="center">
-                        <img src="{{ URL::asset('assets/images/build/mailheader.jpg') }}" alt="Dragon Lancers Creative Agency" width="100%" style="display: block;" />
+                    <td align="center"><img src={{ ('assets/images/build/mailheader.jpg') }} alt="Dragon Lancers Creative Agency" width="100%" style="{display: block;}" /></td>
+                </tr>
+                <tr>
+                    <td style="{background-color: #FCFCFC; padding-left: 10%; padding-right: 10%}">
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                            <tr>
+                                <td style="{padding-top: 5%; padding-bottom: 10%; font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 24px; line-height: 20px;}">New inquiry from the website!</td>
+                            </tr>
+                            <tr>
+                                <td style="{font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 16px; line-height: 20px;}">From {{$name}},</td>
+                            </tr>
+                            <tr>
+                                <td style="{padding-top: 5%; padding-bottom: 10%; font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 16px; line-height: 20px;}">{{$messages}}</td>
+                            </tr>
+                        </table>
                     </td>
                 </tr>
-                <td bgcolor="#fcfcfc" style="padding-left: 10%; padding-right: 10%;">
-                    <table border="0" cellpadding="0" cellspacing="0" width="100%">
-                        <tr>
-                            <td style="padding-top: 5%; padding-bottom: 10%; font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 24px; line-height: 20px;">
-                                New inquiry from the website!
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 16px; line-height: 20px;">
-                                From {{$name}},
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="padding-top: 5%; padding-bottom: 10%; font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 16px; line-height: 20px;">
-                            {{$messages}}
-                            </td>
-                        </tr>
-
-                    </table>
-                    <tr>
-                        <td bgcolor="#000000" height="50" style="padding-left: 10%; padding-right: 10%;">
-                            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="color: #ffffff">
-                                <tr>
-                                    <td width="75%" style="font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 14px; line-height: 20px;">
-                                        &#169; Dragon Lancers {{ date('Y') }}
-                                    </td>
-                                    <td width="25%" align="right">
-                                        <table border="0" cellpadding="0" cellspacing="0">
-                                            <tr>
-                                                <td>
-                                                    <a href="https://www.facebook.com/DragonLancers">
-                                                        <img src={{ URL::asset("assets/images/build/facebook.png") }} alt="Facebook" width="50" height="50" style="display: block;" border="0"/>
-                                                    </a>
-                                                </td>
-                                                <td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
-                                                <td>
-                                                    <a href="https://twitter.com/dragonlancersco">
-                                                        <img src={{ URL::asset("assets/images/build/twitter.png") }} alt="Twitter" width="50" height="50" style="display: block;" border="0"/>
-                                                    </a>
-                                                </td>
-                                                <td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
-                                                <td>
-                                                    <a href="https://plus.google.com/111624196448495342344">
-                                                        <img src={{ URL::asset("assets/images/build/googleplus.png") }} alt="Google+" width="50" height="50" style="display: block;" border="0"/>
-                                                    </a>
-                                                </td>
-                                            </tr>
-                                        </table>
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
+                <tr>
+                    <td height="50" style="{background-color: #000000; padding-left: 10%; padding-right: 10%}">
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="{color: #ffffff}">
+                            <tr>
+                                <td width="75%" style="{font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 14px; line-height: 20px;}">© Dragon Lancers {{ date('Y') }}</td>
+                                <td width="25%" align="right">
+                                    <table border="0" cellpadding="0" cellspacing="0">
+                                        <tr>
+                                            <td><a href="https://www.facebook.com/DragonLancers"><img src={{ ("assets/images/build/facebook.png") }} alt="Facebook" width="50" height="50" class="c1" border="0" /></a></td>
+                                            <td style="{font-size: 0; line-height: 0;}" width="20"> </td>
+                                            <td><a href="https://twitter.com/dragonlancersco"><img src={{ ("assets/images/build/twitter.png") }} alt="Twitter" width="50" height="50" class="c1" border="0" /></a></td>
+                                            <td style="{font-size: 0; line-height: 0;}" width="20"> </td>
+                                            <td><a href="https://plus.google.com/111624196448495342344"><img src={{ ("assets/images/build/googleplus.png") }} alt="Google+" width="50" height="50" class="c1" border="0" /></a></td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
             </table>
-
         </td>
     </tr>
 </table>
-
 </body>
 </html>
--- app/views/emails/welcome.blade.php
+++ app/views/emails/welcome.blade.php
@@ -1,84 +1,81 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="generator" content="HTML Tidy for Linux (vers 25 March 2009), see www.w3.org" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Welcome to Dragon Lancers!</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <style type="text/css">
+        /*<![CDATA[*/
+        body.c15 {margin: 0; padding: 0;}
+        td.c14 {padding: 20px 0 30px 0;}
+        table.c13 {border-collapse: collapse;}
+        td.c12 {background-color: #000000; padding-left: 10%; padding-right: 10%}
+        table.c11 {color: #ffffff}
+        td.c10 {font-size: 0; line-height: 0;}
+        td.c9 {font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 14px; line-height: 20px;}
+        td.c8 {background-color: #FCFCFC; padding-left: 10%; padding-right: 10%}
+        td.c7 {padding-top: 10%; font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 11px; line-height: 15px;}
+        a.c6 {color: #000000;}
+        span.c5 {color: #000000}
+        td.c4 {padding-top: 5%; padding-bottom: 10%; font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 16px; line-height: 20px;}
+        td.c3 {font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 16px; line-height: 20px;}
+        td.c2 {padding-top: 5%; padding-bottom: 10%; font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 24px; line-height: 20px;}
+        img.c1 {display: block;}
+        /*]]>*/
+    </style>
 </head>
-
-<body style="margin: 0; padding: 0;">
+<body class="c15">
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
     <tr>
-        <td style="padding: 20px 0 30px 0;">
-            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;">
+        <td class="c14">
+            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" class="c13">
                 <tr>
-                    <td align="center">
-                        <img src={{ URL::asset('assets/images/mailheader.jpg') }} alt="Dragon Lancers Creative Agency" width="100%" style="display: block;" />
+                    <td align="center"><img src={{ URL::asset('assets/images/mailheader.jpg') }} alt="Dragon Lancers Creative Agency" width="100%" class="c1" /></td>
+                </tr>
+                <tr>
+                    <td class="c8">
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                            <tr>
+                                <td class="c2">Welcome to the Dragon Family!</td>
+                            </tr>
+                            <tr>
+                                <td class="c3">Dear name,</td>
+                            </tr>
+                            <tr>
+                                <td class="c4">Thank you for inquiring us about our services, you have taken the first step, in the right direction towards a great online presence.<br />
+                                    <br />
+                                    Each and every client is important to us, we do our very best to design, develop and launch the very best for each client. All of our clients are a part of our Dragon Family<br />
+                                    <br />
+                                    We will get in touch with you shortly.</td>
+                            </tr>
+                            <tr>
+                                <td class="c3">Kind regards,<br />
+                                    Dragon Lancers</td>
+                            </tr>
+                            <tr>
+                                <td class="c7">Unfortunately, this email is an automated notification,<br />
+                                    which is unable to receive replies, we will get back to you as soon as possible.<br />
+                                    We are happy to help you with any questions or concerns you may have.<br />
+                                    Please contact us directly 24/7 via <a class="c6" href="http://www.dragonlancers.com/#contact"><span class="c5">dragonlancers.com/#contact</span></a> or <a class="c6" href="mailto:hello@dragonlancers.com?Subject=Hello%20Again" target="_top">hello@dragonlancers.com</a>.</td>
+                            </tr>
+                        </table>
                     </td>
                 </tr>
                 <tr>
-                    <td bgcolor="#fcfcfc" style="padding-left: 10%; padding-right: 10%;">
-                        <table border="0" cellpadding="0" cellspacing="0" width="100%">
-                        <tr>
-                            <td style="padding-top: 5%; padding-bottom: 10%; font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 24px; line-height: 20px;">
-                                Welcome to the Dragon Family!
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 16px; line-height: 20px;">
-                                Dear {{$name}},
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="padding-top: 5%; padding-bottom: 10%; font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 16px; line-height: 20px;">
-                                Thank you for inquiring us about our services, you have taken the first step, in the right direction towards a great online presence. <br/> <br/>
-                                Each and every client is important to us, we do our very best to design, develop and launch the very best for each client. All of our clients are a part of our Dragon Family
-                                <br/> <br/>
-                                We will get in touch with you shortly.
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 16px; line-height: 20px;">
-                                Kind regards, <br/>
-                                Dragon Lancers
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="padding-top: 10%; font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 11px; line-height: 15px;">
-                                Unfortunately, this email is an automated notification, <br/>
-                                which is unable to receive replies, we will get back to you as soon as possible. <br/>
-                                We are happy to help you with any questions or concerns you may have. <br/>
-                                Please contact us directly 24/7 via <a style="color: #000000;" href="http://www.dragonlancers.com/#contact"><font color="#000000">dragonlancers.com/#contact</font></a>  or <a style="color: #000000;" href="mailto:hello@dragonlancers.com?Subject=Hello%20Again" target="_top">hello@dragonlancers.com</a>.
-                            </td>
-                        </tr>
-                    </table>
-                <tr>
-                    <td bgcolor="#000000" height="50" style="padding-left: 10%; padding-right: 10%;">
-                        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="color: #ffffff">
+                    <td height="50" class="c12">
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%" class="c11">
                             <tr>
-                                <td width="75%" style="font-family: Helvetica, Arial, Verdana, Trebuchet MS, sans-serif; font-size: 14px; line-height: 20px;">
-                                    &#169; Dragon Lancers {{ date('Y') }}
-                                </td>
+                                <td width="75%" class="c9">© Dragon Lancers {{ date('Y') }}</td>
                                 <td width="25%" align="right">
                                     <table border="0" cellpadding="0" cellspacing="0">
                                         <tr>
-                                            <td>
-                                                <a href="https://www.facebook.com/DragonLancers">
-                                                    <img src={{ URL::asset("assets/images/facebook.png") }}  width="50" height="50" style="display: block;" border="0" alt="Facebook"/>
-                                                </a>
-                                            </td>
-                                            <td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
-                                            <td>
-                                                <a href="https://twitter.com/dragonlancersco">
-                                                    <img src={{ URL::asset("assets/images/twitter.png") }}  width="50" height="50" style="display: block;" border="0" alt="Twitter"/>
-                                                </a>
-                                            </td>
-                                            <td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
-                                            <td>
-                                                <a href="https://plus.google.com/111624196448495342344">
-                                                    <img src={{ URL::asset("assets/images/googleplus.png") }}  width="50" height="50" style="display: block;" border="0" alt="Google+"/>
-                                                </a>
-                                            </td>
+                                            <td><a href="https://www.facebook.com/DragonLancers"><img src={{ ("assets/images/facebook.png") }} width="50" height="50" class="c1" border="0" alt="Facebook" /></a></td>
+                                            <td class="c10" width="20"> </td>
+                                            <td><a href="https://twitter.com/dragonlancersco"><img src={{ ("assets/images/twitter.png") }} width="50" height="50" class="c1" border="0" alt="Twitter" /></a></td>
+                                            <td class="c10" width="20"> </td>
+                                            <td><a href="https://plus.google.com/111624196448495342344"><img src={{ ("assets/images/googleplus.png") }} width="50" height="50" class="c1" border="0" alt="Google+" /></a></td>
                                         </tr>
                                     </table>
                                 </td>
@@ -86,12 +83,9 @@
                         </table>
                     </td>
                 </tr>
-                </tr>
             </table>
-
         </td>
     </tr>
 </table>
-
 </body>
 </html>
